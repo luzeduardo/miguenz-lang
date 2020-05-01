1a65c25bec3d1a96babee7b48450534db17ea307
Eduardo Luz <luz.eduardo@gmail.com>
2020-05-01 19:48:25 -0300

    feat(lexer): add helper to handle next char and increase position in string being evaluated

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,3 +11,13 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 //sets ASCII for NUL char
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}

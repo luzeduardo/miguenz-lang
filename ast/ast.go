0887997456ff238f2d82a20fcf870c87b1e63b2d
Eduardo Luz <luz.eduardo@gmail.com>
2020-05-19 08:50:46 -0300

    feat(ast): add simple initial AST structure for a LET statement for variable binding

--- ast/ast.go
+++ ast/ast.go
@@ -1,7 +1,9 @@
 package ast
 
+import "miguenz-lang/token"
+
 type Node interface {
-	TokenListeral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
@@ -21,8 +23,32 @@ type Program struct {
 
 func (p *Program) TokenListeral() string {
 	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenListeral()
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+// Identifier implements Expression to simplify the reuse of variable name bind as an Expression after
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+// LetStatement Name holds the Name of the binding also the Expression that produces value
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
 }
